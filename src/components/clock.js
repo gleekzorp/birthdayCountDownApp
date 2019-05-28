da90f918d661cac099d7219a64b2bf16663bb3c7
gleekzorp <gleek.dan@gmail.com>
2019-05-28 14:00:24 -0600

    Implemented age feature

--- src/components/clock.js
+++ src/components/clock.js
@@ -17,14 +17,14 @@ export default class Clock extends Component {
         
         var bday = new Date(birthday)
         let today = new Date();
-
+        
         var distance = bday.getTime() - today.getTime();
-
+        
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+        
         return {
             'days': days,
             'hours': hours,
@@ -32,6 +32,16 @@ export default class Clock extends Component {
             'seconds': seconds
         }
     }
+    
+    getAge = function() {
+        var bday = new Date(this.birthday)
+        let today = new Date();
+
+        var distance = today.getTime() - bday.getTime();
+        var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var yearsOld = Number((daysOld/365).toFixed(0))
+        return yearsOld
+    }.bind(this)
 
     componentDidMount() {
             this.timer = setInterval(() => {
@@ -48,10 +58,15 @@ export default class Clock extends Component {
         console.log('RENDERING');
         return (
             <div>
-                <div>DAYS {data.days}</div>
-                <div>HRS {data.hours}</div>
-                <div>MINS {data.minutes}</div>
-                <div>SECS {data.seconds}</div>
+                <div>
+                    <div>DAYS {data.days}</div>
+                    <div>HRS {data.hours}</div>
+                    <div>MINS {data.minutes}</div>
+                    <div>SECS {data.seconds}</div>
+                </div>
+                <div>
+                    {<h4>Remaining until you are {this.getAge()}</h4>}
+                </div>
             </div>
         )
 
