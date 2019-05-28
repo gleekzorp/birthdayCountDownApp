f2239bc5e7953df809a916f950e9e989b2b4d62a
gleekzorp <gleek.dan@gmail.com>
2019-05-28 11:52:37 -0600

    Setup Datepicker

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -6,13 +6,27 @@ import "react-datepicker/dist/react-datepicker.css";
 export default class BirthdayForm extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            startDate: new Date()
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
     }
 
     render() {
         return (
             <div>
                 <h1>Birthday Form Component</h1>
-                <DatePicker />
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
             </div>
         )
     }
